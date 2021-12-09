82e0bbdb9ff8ec4214d300a790dab517aaddc463
NicolenaStiles <nicolena.stiles@gmail.com>
2021-12-08 21:50:40 -0500

    First commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+### Rust-based Simple Scheduler
+
+Haha, placeholder.
---
+++ src/main.rs
@@ -0,0 +1,19 @@
+// Entry point for rusty scheduler
+
+// reference:
+// https://rust-lang-nursery.github.io/rust-cookbook/datetime.html
+use chrono::prelude::*;
+
+fn main() {
+    println!("Hello, scheduler!");
+
+    // testing getting current datetime info
+    let dt_now : DateTime<Utc> = Utc::now();
+
+    // setting datetime info
+    let birthdate = Utc.ymd(1995, 5, 22).and_hms(12, 50, 8); // `2014-07-08T09:10:11Z`
+
+    // printing/unravelling format
+    println!("{:?}", dt_now.format("%Y-%m-%d %H:%M:%S").to_string());
+    println!("{:?}", birthdate.format("%Y-%m-%d %H:%M:%S").to_string());
+}
